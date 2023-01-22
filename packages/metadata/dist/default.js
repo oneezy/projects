98337c4cbe629d855d4d9b7c1b534c282fe3c781
Justin ONeill <justinoneill2007@gmail.com>
2023-01-22 01:50:21 -0600

    chore: updates

--- packages/metadata/dist/default.js
+++ packages/metadata/dist/default.js
@@ -1,9 +1,9 @@
-export const APP_URL = 'https://Oneezy.io';
+export const APP_URL = 'https://oneezy.com';
 export const APP_NAME = 'Oneezy';
-export const APP_TITLE = 'Make Crypto Happen';
-export const APP_AUTHOR = "Scavenger Team"
+export const APP_TITLE = 'Frontend Developer';
+export const APP_AUTHOR = "Justin O'Neill"
 export const APP_SEPARATOR = '|';
-export const APP_DESCRIPTION = "Oneezy™ is your personal crypto trading co-pilot.";
+export const APP_DESCRIPTION = "Let oneezy' guide you towards technology enlightment";
 // export const FB_ID = '902427283166466';
 // export const FB_ADMINS = '1271441153';
 // export const DISCORD_HANDLE = 'oneezy';
